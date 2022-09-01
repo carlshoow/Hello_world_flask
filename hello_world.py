9fa065df3ab283fbfd6c11bf089f99f72b76c8e8
Oliver_S <90482439+carlshoow@users.noreply.github.com>
2022-09-01 23:17:01 +0200

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from markupsafe import escape
 
 # Creating an instance of Flask class
 app = Flask(__name__)
 
 #Using the decorator to tell python what URL should trigger our function
-@app.route("/")
-
+@app.route("/owner")
 def hello_world():
+    name = "Carlos"
     #Returns the message we want to display in the user’s browser
-    return "<h1>Hello World</h1>"
+    return f" Hello {escape(name)}!"
+
+
+#Main page
+@app.route("/main_page")
+def Tests():
+    return "<h>THIS IS THE MAIN PAGE<h>"
+
